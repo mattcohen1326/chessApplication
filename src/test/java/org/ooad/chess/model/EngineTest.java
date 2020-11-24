88f98d59bc777f4f90ebfe4117df327afd187a20
mattcohen1326 <maco2574@colorado.edu>
2020-11-24 15:17:54 -0500

    some more pawn tests

--- src/test/java/org/ooad/chess/model/EngineTest.java
+++ src/test/java/org/ooad/chess/model/EngineTest.java
@@ -13,7 +13,24 @@ public class EngineTest {
             Assert.assertEquals(false,engine.isBlocked(b.getPiece(engine.stringifyMove(b.LENGTH-1,i)).getMovement().movePath(engine.stringifyMove(b.LENGTH-1,i),engine.stringifyMove(b.LENGTH-2,i))));
             Assert.assertEquals(true,b.getPiece(engine.stringifyMove(b.LENGTH-1,i)).getMovement().movePossible(engine.stringifyMove(b.LENGTH-1,i),engine.stringifyMove(b.LENGTH-2,i),true,false));
         }
+    }
+    @Test
+    public void testEnPassant(){
+        b.movePiece("B2","B3");
+        b.movePiece("B3","B4");
+        b.movePiece("B4","B5");
+        b.movePiece("A7","A5");
+        Assert.assertEquals(true, engine.isEliminating("B5","A6"));
+        b = Board.filledBoard();
 
-
+    }
+    @Test
+    public void checkPawnForwardTwo(){
+        for(int i = 1; i <= b.LENGTH; i++){
+            Assert.assertEquals(false,engine.isBlocked(b.getPiece(engine.stringifyMove(2,i)).getMovement().movePath(engine.stringifyMove(2,i),engine.stringifyMove(4,i))));
+            Assert.assertEquals(true,b.getPiece(engine.stringifyMove(2,i)).getMovement().movePossible(engine.stringifyMove(2,i),engine.stringifyMove(4,i),true,false));
+            Assert.assertEquals(false,engine.isBlocked(b.getPiece(engine.stringifyMove(b.LENGTH-1,i)).getMovement().movePath(engine.stringifyMove(b.LENGTH-1,i),engine.stringifyMove(b.LENGTH-3,i))));
+            Assert.assertEquals(true,b.getPiece(engine.stringifyMove(b.LENGTH-1,i)).getMovement().movePossible(engine.stringifyMove(b.LENGTH-1,i),engine.stringifyMove(b.LENGTH-3,i),true,false));
+        }
     }
 }
